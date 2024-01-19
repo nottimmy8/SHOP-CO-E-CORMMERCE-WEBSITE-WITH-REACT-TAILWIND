756234d529d2c027db7bebbd99af2b39bbba958e
oyewole timilehin <oyewoleoluwatimilehin2@gmail.com>
2024-01-19 04:39:06 -1100

    update

---
+++ src/components/Features.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Features() {
+  return (
+    <div className="w-full px-4 bg-black h-[300px]  ">
+      <div className="flex items-center">
+        <img src="/Group.png" alt="" />
+        <img src="/Group1.png" alt="" />
+        <img src="/gucci-logo-11.png" alt="" />
+        <img src="/prada-logo-21.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Features;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,13 +2,24 @@ import React from "react";
 
 function Hero() {
   return (
-    <div>
-      <div className="flex flex-col lg:flex-row">
+    <div className="max-w-[1360px] mx-auto px-4 ">
+      <div className="max-h-[600px]  relative flex flex-col  lg:flex-row">
         {/* Overlay */}
-        <div>
-          <h1>nghnjgnksmgj</h1>
+        <div className="absolute w-full h-full text-black max-h-[600px]flex flex-col justify-center sm:w-full md:w-[700px] lg:w-[700px] ">
+          <h1 className=" px-4 text-4xl sm:text-2xl md:text-5xl lg:text-6xl font-bold flex flex-col ">
+            FIND CLOTHES <span> THAT MATCHES </span>
+            <span>YOUR STYLE</span>
+          </h1>
+          <p className="px-4 text-2xl">
+            Browse through our diverse range of meticulously crafted garments,
+            designed to bring out your individuality and cater to your sense of
+            style.
+          </p>
+          <button className="bg-black ml-4 text-white hidden md:flex items-center py-2 rounded-full ">
+            Shop Now
+          </button>
         </div>
-        <img src="/Rectangle2.png" alt="" className="w-full max-h-[500px]  " />
+        <img src="/Rectangle2.png" alt="" className="w-full max-h-[600px]  " />
       </div>
     </div>
   );
